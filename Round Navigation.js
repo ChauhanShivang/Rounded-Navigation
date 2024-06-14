5852eb21a5e390dec4d76c9c569661dd11f497e1
ChauhanShivang <chauhanshivang2003@gmail.com>
2024-06-14 15:49:31 +0530

    Round Navigation

---
+++ Round Navigation.js
@@ -0,0 +1,4 @@
+document.querySelector(".navbar-btn").addEventListener("click", () => {
+    document.querySelector(".navbar").classList.toggle("change");
+    document.querySelector(".navbar-btn").classList.toggle("change");
+})
